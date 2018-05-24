74bd553b472f35e46f6431c1e1e59e5941cdedb5
nicholashaynes1 <nicholashaynes1@gmail.com>
2018-05-24 02:29:09 -0600

    Implemented the insert email function.

--- InstagramScrapper.py
+++ InstagramScrapper.py
@@ -3,11 +3,22 @@ from time import sleep
 
 # Defines the broweser we are using.
 driver = webdriver.Chrome('C:/Users/nick/InstaPy/assets/chromedriver')
-
 # Navigate to instagram
 driver.get('https://www.instagram.com')
 
-# Finds the email input field
-emailInput =driver.find_elements_by_xpath("//input[@name='emailOrPhone' and @value='']")[0]
-# clicks the email inputField
-emailInput.click()
+def enterEmail():
+
+	# delays checking the javascript elements of a page so it is fully loaded.
+	sleep(.2)
+
+	# Finds the email input field by scraping the js
+	emailInput = driver.find_elements_by_xpath("//input[@name='emailOrPhone' and @value='']")[0]
+
+	# clicks the email inputField
+	emailInput.click()
+
+	# Sends the desired email to the email text box
+	emailInput.send_keys("Nick")
+	return;
+
+enterEmail()
